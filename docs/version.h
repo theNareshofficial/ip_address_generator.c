b28cc5390344790f12ed9256a2883e55cad7006a
thenareshofficial <thenareshoffcial@gmail.com>
2023-11-11 10:33:42 -0800

    Show Version command

---
+++ docs/version.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define VERSION "1.0"
+#define Developer "thenareshofficial(Naresh)"
+#define LINK "https://github.com/theNareshofficial/https://github.com/theNareshofficial/ip_address_generator.c"
+
+int show_version()
+{
+        printf("\033[1;33mIP generator version : \033[1;32m%s\n",VERSION);
+        printf("\033[1;33mDeveloper : \033[1;32m%s\n",Developer);
+        printf("\033[1;33mGithub link : \033[1;32m%s\n",LINK);
+}
